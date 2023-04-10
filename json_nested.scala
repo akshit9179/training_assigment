c3f38ad574d013089972b94481f08e461cf5e5fa
akshit9179 <akshit.anand@kpipartners.com>
2023-04-10 11:14:05 +0530

    commit json

---
+++ json_nested.scala
@@ -0,0 +1,49 @@
+// Databricks notebook source
+val df = spark.read.option("multiline", "true").json("dbfs:/FileStore/shared_uploads/akshitnand@gmail.com/test_json.json")
+
+// COMMAND ----------
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.{ArrayType, StructType}
+ 
+def flattenDataFrame(df: DataFrame): DataFrame = {
+  val fields = df.schema.fields
+  val fieldNames = fields.map(x => x.name)
+ 
+  for (i <- fields.indices) {
+    val field = fields(i)
+    val fieldType = field.dataType
+    val fieldName = field.name
+    fieldType match {
+      case _: ArrayType =>
+        val fieldNamesExcludingArray = fieldNames.filter(_ != fieldName)
+        val fieldNamesAndExplode = fieldNamesExcludingArray ++ Array(
+          s"explode_outer($fieldName) as $fieldName"
+        )
+        val explodedDf = df.selectExpr(fieldNamesAndExplode: _*)
+        return flattenDataFrame(explodedDf)
+      case structType: StructType =>
+        val childFieldNames =
+          structType.fieldNames.map(childname => s"$fieldName.$childname")
+        val newFieldNames = fieldNames.filter(_ != fieldName) ++ childFieldNames
+ 
+        val renamedCols =
+          newFieldNames.map { x =>
+            col(x.toString).as(x.toString.replace(".", "_"))
+          }
+ 
+        val explodedDf = df.select(renamedCols: _*)
+        return flattenDataFrame(explodedDf)
+      case _ =>
+    }
+  }
+ 
+  df
+}
+ 
+val ls = flattenDataFrame(df)
+
+// COMMAND ----------
+
+display(ls)
